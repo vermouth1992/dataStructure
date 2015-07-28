2978ec9c4459432d053c746075b478d2f74f8e39
Chi Zhang <czhangseu@gmail.com>
2015-07-27 18:52:42 -0700

    add GNU license description

--- testProgram/ListNode.h
+++ testProgram/ListNode.h
@@ -2,9 +2,14 @@
 //  ListNode.h
 //  testProgram
 //
-//  Created by Chi Zhang on 7/23/15.
-//  Copyright (c) 2015 University of Southern California. All rights reserved.
-//
+//  Created by Chi Zhang on 7/13/15.
+//  This is a self-paced data structure exercises described in C++
+/* Copyright (C) <2015> <Chi Zhang>
+ This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ If you have any questions, please email zhan527@usc.edu
+ */
 
 #ifndef __testProgram__ListNode__
 #define __testProgram__ListNode__
@@ -26,6 +31,22 @@ public:
     ListNode<T>* insertAsSucc(T const& e);
 };
 
+template <typename T>
+ListNode<T>* ListNode<T>::insertAsPred(const T &e)
+{
+    ListNode<T>* x = new ListNode<T>(e, pred, this);
+    pred->succ = x;
+    pred = x;
+    return x;
+}
 
+template <typename T>
+ListNode<T>* ListNode<T>::insertAsSucc(const T &e)
+{
+    ListNode<T>* x = new ListNode<T>(e, this, succ);
+    succ->pred = x;
+    succ = x;
+    return x;
+}
 
 #endif /* defined(__testProgram__ListNode__) */
--- testProgram/main.cpp
+++ testProgram/main.cpp
@@ -1,18 +1,20 @@
-//
 //  main.cpp
 //  testProgram
 //
 //  Created by Chi Zhang on 7/13/15.
-//  Copyright (c) 2015 University of Southern California. All rights reserved.
-//
+//  This is a self-paced data structure exercises described in C++
+/* Copyright (C) <2015> <Chi Zhang>
+ This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ If you have any questions, please email zhan527@usc.edu
+*/
 
 #include <iostream>
 #include "Fibonacci.h"
 #include "MyVector.h"
 #include "List.h"
-#include "List.cpp"
 #include "ListNode.h"
-#include "ListNode.cpp"
 #include "Stack.h"
 #include "math.h"
 
