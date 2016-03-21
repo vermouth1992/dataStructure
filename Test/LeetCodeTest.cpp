891257ff308f98a459d40c2cff36bed4bf7e48a7
vermouth1992 <czhangseu@gmail.com>
2016-03-21 12:35:24 -0700

    finish two sum, three sum still bugs

--- Test/LeetCodeTest.cpp
+++ Test/LeetCodeTest.cpp
@@ -12,13 +12,55 @@
 
 using namespace std;
 
-TEST(twoSum, basic_test)
+TEST(sum, twoSum)
 {
-    int myints[] = {2,16,29,77};
+    int myints[] = {0, 4, 3, 0};
     vector<int> test_vector (myints, myints + sizeof(myints) / sizeof(int) );
     vector<int> target_index;
-    target_index.push_back(1);
+    target_index.push_back(0);
     target_index.push_back(3);
-    vector<int> result_index = twoSum(test_vector, target_index[0] + target_index[1]);
+    vector<int> result_index = twoSum(test_vector, test_vector[target_index[0]] + test_vector[target_index[1]]);
     EXPECT_EQ(target_index, result_index);
 }
+
+TEST(sum, twoSumAll)
+{
+    int myints[] = {2, 4, 3, 5};
+    vector<int> test_vector (myints, myints + sizeof(myints) / sizeof(int) );
+    vector<int> target_index_1;
+    target_index_1.push_back(0);
+    target_index_1.push_back(3);
+    vector<int> target_index_2;
+    target_index_2.push_back(1);
+    target_index_2.push_back(2);
+    vector<vector<int> > target_index;
+    target_index.push_back(target_index_2);
+    target_index.push_back(target_index_1);
+    vector<vector<int> > result_index = twoSumAll(0, test_vector, 7);
+    EXPECT_EQ(target_index, result_index);
+}
+
+TEST(sum, threeSum1)
+{
+    int myints[] = {7,-1,14,-12,-8,7,2,-15,8,8,-8,-14,-4,-5,7,9,11,-4,-15,-6,1,-14,4,3,10,-5,2,1,6,11,2,-2,-5,-7,-6,2,-15,11,-6,8,-4,2,1,-1,4,-6,-15,1,5,-15,10,14,9,-8,-6,4,-6,11,12,-15,7,-1,-9,9,-1,0,-4,-1,-12,-2,14,-9,7,0,-3,-4,1,-2,12,14,-10,0,5,14,-1,14,3,8,10,-8,8,-5,-2,6,-11,12,13,-7,-12,8,6,-13,14,-2,-5,-11,1,3,-6};
+    vector<int> test_vector (myints, myints + sizeof(myints) / sizeof(int) );
+    vector<int> target_index_1 = {-1, 0, 1};
+    vector<int> target_index_2 = {-1, -1, 2};
+    vector<vector<int>> expected = {target_index_1, target_index_2};
+    vector<vector<int>> actual = threeSum(test_vector);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(sum, threeSum2)
+{
+    int myints[] = {0, 0, 0};
+    vector<int> test_vector (myints, myints + sizeof(myints) / sizeof(int) );
+    vector<int> target_index_1 = {0, 0, 0};
+    vector<vector<int>> expected = {target_index_1};
+    vector<vector<int>> actual = threeSum(test_vector);
+    EXPECT_EQ(expected, actual);
+}
+
+
+
+
--- testProgram/LeetCode.hpp
+++ testProgram/LeetCode.hpp
@@ -10,13 +10,64 @@
 #define LeetCode_h
 
 #include <vector>
-// #1, two sum
+#include <map>
+
 using namespace std;
 
+// #1, two sum
+/* using map, total time complexity is O(n) + O(n) = O(n) */
 vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> result = {1, 3};
+    std::map<int, int> nums_map;
+    // O(n) to iterate
+    vector<int> result;
+    std::map<int, int>::iterator map_it;
+    for (int i = 0; i < nums.size(); i++) {
+        map_it = nums_map.find(target - nums[i]);
+        if (map_it != nums_map.end()) {
+            return {map_it->second, i};
+        }
+        nums_map[nums[i]] = i;
+    }
     return result;
 }
 
+// #15, 3Sum
+// helper function, the output two-d vector should be a set
+vector<vector<int> > twoSumAll(int starting, vector<int>& nums, int target) {
+    std::map<int, int> nums_map;
+    vector<vector<int> > result;
+    // O(n) to iterate
+    std::map<int, int>::iterator map_it;
+    for (int i = starting; i < nums.size(); i++) {
+        if (i != starting && nums[i] == nums[i-1]) {
+            continue;
+        }
+        map_it = nums_map.find(target - nums[i]);
+        if (map_it != nums_map.end()) {
+            result.push_back({map_it->second, i});
+        }
+        nums_map[nums[i]] = i;
+    }
+    return result;
+}
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+    // sort the array
+    std::sort(nums.begin(), nums.end());
+    vector<vector<int> > result;
+    vector<vector<int> > twoSumResult;
+    for (int i = 0; i < nums.size(); i++) {
+        // step over duplicate elements
+        if (i != 0 && nums[i] == nums[i-1]) {
+            continue;
+        }
+        twoSumResult = twoSumAll(i + 1, nums, -nums[i]);
+        for (int j = 0; j < twoSumResult.size(); j++) {
+            vector<int> singleTwoResult = twoSumResult[j];
+            result.push_back({nums[i], nums[singleTwoResult[0]], nums[singleTwoResult[1]]});
+        }
+    }
+    return result;
+}
 
 #endif /* LeetCode_h */
